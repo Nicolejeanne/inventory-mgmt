bded254b950e9d7937bc984559ea2194d80b13b1
Nicolejeanne <nicolejeanne@users.noreply.github.com>
2019-01-13 21:36:37 -0600

    switched to npm from yarn

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Nav from "./components/Nav";
-// import SideMenu from "./components/SideMenu";
+import SideMenu from "./components/SideMenu";
 
 function App() {
   return (
     <div>
       <Nav />
-      {/* <SideMenu/> */}
+      <SideMenu/>
     </div>
   );
 }
