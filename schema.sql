672c4893f2930b4dd9f04cceaaf1be8d03c1d864
Nicolejeanne <nicolejeanne@users.noreply.github.com>
2019-01-17 22:31:48 -0600

    created schema and seeds files

---
+++ schema.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE partsInventory_db;
+
+USE partsInventory_db;
+
+CREATE TABLE partDefinition (
+    part_id INT NOT NULL AUTO_INCREMENT,
+    partNumber VARCHAR(100) NOT NULL,
+    partType VARCHAR(50) NOT NULL,
+    partDescription TEXT NOT NULL,
+    manufacturer VARCHAR(100) NOT NULL,
+    unitType varchar(10) NOT NULL, 
+    PRIMARY KEY (part_id)
+);
+
+CREATE TABLE vendors (
+    vendor_id INT NOT NULL AUTO_INCREMENT,
+    vendorName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (vendor_id)
+);
+
+CREATE TABLE partEntry (
+    partEntry_id INT NOT NULL AUTO_INCREMENT,
+    orderType VARCHAR(100) NOT NULL,
+    orderNumber VARCHAR(50) NOT NULL,
+    vendor VARCHAR(255) NOT NULL,
+    FOREIGN KEY fk_vendor(vendor_id),
+    REFERENCES vendors(vendor_id),
+    project VARCHAR(50) NOT NULL,
+    partNumberEntry VARCHAR(100) NOT NULL,
+    partRevision VARCHAR(5),
+    quantity INT NOT NULL,
+    conforming boolean NOT NULL,
+    room VARCHAR(20) NOT NULL,
+    cabinet VARCHAR(20) NOT NULL,
+    bin VARCHAR(20) NOT NULL,
+    package INT NOT NULL AUTO_INCREMENT,
+    added DATE NOT NULL,
+    addedBy VARCHAR NOT NULL,
+    FOREIGN KEY fk_part(part_id),
+    REFERENCES partDefinition(part_id),
+    PRIMARY KEY (partEntry_id)
+);
---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO partDefinition (partNumber, partType, partDescription, manufacturer, unitType)
+VALUES ();
+
+
+
+INSERT INTO vendors (vendorName)
+VALUES ();
+
+
+INSERT INTO partEntry (orderType, orderNumber, vendor, project, partNumberEntry, partRevision, quantity, conforming, room, cabinet, bin)
+VALUES();
